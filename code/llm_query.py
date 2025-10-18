f37aaa8f9d59f3ec61350c5d50072a50146d5e1f
Achillegillig <120458968+Achillegillig@users.noreply.github.com>
2025-10-19 00:13:54 +0200

    add debug mode to not burn jonathan cash

--- code/llm_query.py
+++ code/llm_query.py
@@ -3,9 +3,23 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+debug = bool(int(os.getenv("DEBUG", "0")))
+print("Debug mode:", debug)
+if debug:
+    api_key = "lm-studio"
+    base_url = os.getenv("LMSTUDIO_BASE_URL", "pippo")
+    model = os.getenv("DEBUG_MODEL", "pippo")
+    # does not do anything really.
+else:
+    api_key = os.getenv("OPENROUTER_API_KEY")
+    base_url = "https://openrouter.ai/api/v1"
+    model = "openai/gpt-4o"
+
+print(base_url)
 client = OpenAI(
-    api_key=os.getenv("OPENROUTER_API_KEY"),
-    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+    base_url=base_url,
 )
 
 def call_api(prompt: list, role:str) -> dict:
@@ -17,10 +31,12 @@ def call_api(prompt: list, role:str) -> dict:
             message.append(msg)
         else:
             message.append({"role": "user", "content": msg["content"]})
-        
-    response = client.chat.completions.create(
-        model="openai/gpt-4o",
-            messages=message
-    )
-    formatted_response = response.choices[0].message.content 
+    if not debug:    
+        response = client.chat.completions.create(
+            model=model,
+                messages=message
+        )
+        formatted_response = response.choices[0].message.content 
+    else:
+        formatted_response = "pippopippou debug"
     return formatted_response 
