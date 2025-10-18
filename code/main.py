c722e4980a00edc971779b4806de5799fcb8bbb0
Jonathan Legrand <jonathan.a.legrand@inria.fr>
2025-10-18 14:40:40 +0200

    Line returns

--- code/main.py
+++ code/main.py
@@ -15,13 +15,12 @@ def main():
     question = "What brings you today?"
     while True:
         response = ask_patient(question)
-
     
         symptoms_score = symptoms_func(response)
         investigator.update_patient_representation(symptoms_score)
         # TODO update clinical reprot
         # investigator.update_clinical_report()
-        
+
         instruction =  investigator.generate_instruction()
 
 
