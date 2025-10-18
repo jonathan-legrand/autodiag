f06888eae9f7e745559710df171506defa367349
Jonathan Legrand <jonathan.a.legrand@inria.fr>
2025-10-18 14:44:04 +0200

    Functioning update

--- code/investigator.py
+++ code/investigator.py
@@ -1,36 +1,26 @@
+
 # %%
 import numpy as np
 import pandas as pd
 
-rng = np.random.default_rng()
-long_desc = pd.read_csv("../data/datalong.csv", index_col=0)
-# %%
+from functools import reduce
 
-long_scores = long_desc.copy()
-long_scores["symptome"] = rng.normal(size=(len(long_desc)))
-# %%
-import seaborn as sns
-import matplotlib.pyplot as plt
+def concat_sentences(x, y):
+    return x + ". " + y
 
-sorted_disorders = long_scores.groupby("disorder").sum(numeric_only=True).sort_values(
-    by="symptome", ascending=False
-)
-most_likely_disorder = sorted_disorders.index[0]
-
-# %%
-res = long_desc[long_desc.code == most_likely_disorder]
-res
-# %%
 
 class Investigator:
-    def __init__(self, n_switch_cycles=5):
+    def __init__(self, n_switch_cycles=5, data_path="../data/datalong.csv"):
+
         self.n_switch_cycles = n_switch_cycles
         self.iteration_counter = 0
         self.explore = True
-        self.long_data = pd.read_csv("data/datalong.csv", index_col=0)
-        long_scores = self.long_data.copy()
-        long_scores["score"] = np.zeros(size=(len(long_desc)))
+
+        long_data = pd.read_csv(data_path, index_col=0)
+        long_scores = long_data.copy()
+        long_scores["score"] = np.zeros(shape=(len(long_data)))
         self.long_scores = long_scores
+        self.long_data = long_data
 
         self.conversation_history = []
         self.conversation_summary = []
@@ -43,7 +33,7 @@ class Investigator:
 
 
     def update_patient_representation(self, new_scores):
-        self.long_scores += new_scores
+        self.long_scores["score"] += new_scores["score"]
         self.iteration_counter += 1
 
         # first proxy: n cycles
@@ -53,14 +43,27 @@ class Investigator:
     def generate_instruction(self):
         if self.explore:
             return "Continue the conversation"
-
         most_important_disease = self.long_scores.sort_values(
             by="symptome", ascending=False
         ).loc[0, "code"]
-        self.long_data[long_scores.code == most_important_disease]
-        ## Extract most important symptom
-        # Explore symptoms from same pathology
-        pass
+        self.long_data[self.long_scores.code == most_important_disease]
+        instructions = """
+        "Ask a short question that explores as many of the following symptoms :
+
+        """
+        relevants_symptoms = reduce(concat_sentences, self.long_data[self.long_scores.code == "F60.2"].symptome)
+        return instructions + relevants_symptoms
 
     def compute_score_distribution(self):
         return self.long_scores.groupby("code").sum(numeric_only=True) / self.iteration_counter
+
+# %%
+investigator = Investigator()
+
+fake_update = investigator.long_scores.copy()
+rng = np.random.default_rng()
+fake_update["score"] = rng.normal(size=(len(fake_update)))
+
+print(investigator.long_scores[["code", "score"]].head())
+investigator.update_patient_representation(fake_update)
+print(investigator.long_scores[["code", "score"]].head())
